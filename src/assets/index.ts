dcdb84ade566e77ec9502ba9527881e7ee0811d7
alimoncul <alimoncul@gmail.com>
2025-01-02 00:36:29 +0300

    fix: images are not resolving

---
+++ src/assets/index.ts
@@ -0,0 +1,7 @@
+import vixenImage from './vixen.png';
+import rangersRingImage from './rangersring.png';
+
+export const Images = {
+    vixen: vixenImage,
+    rangersring: rangersRingImage
+}
--- src/components/InventoryCell.tsx
+++ src/components/InventoryCell.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import Item, { ItemRarityType } from "../types";
+import { Images } from "../assets";
 
 interface InventoryCellProps {
   item?: Item;
@@ -8,6 +9,8 @@ interface InventoryCellProps {
 const InventoryCell: React.FC<InventoryCellProps> = (props: InventoryCellProps) => {
   const [isHovered, setIsHovered] = useState(false);
   const rarityColor = props.item && props.item.rarity === ItemRarityType.Unique ? 'text-yellow-400' : props.item && props.item.rarity === ItemRarityType.Epic ? 'text-purple-400' : 'text-blue-400';
+  const image = props.item?.name ? Images[props.item.name.toLowerCase().replace(/[' ]/g, '') as keyof typeof Images] : '';
+
   return (
     <div
       className="bg-gray-700 h-16 w-16 border border-gray-600 rounded-md flex items-center justify-center relative"
@@ -16,7 +19,7 @@ const InventoryCell: React.FC<InventoryCellProps> = (props: InventoryCellProps) 
     >
       {props.item && (
         <img
-          src={`rise-online-calculator/assets/${props.item.name.toLowerCase()}.png`}
+          src={image}
           alt={props.item.name}
           className="h-full w-full object-cover rounded-md"
         />
@@ -24,7 +27,7 @@ const InventoryCell: React.FC<InventoryCellProps> = (props: InventoryCellProps) 
       {props.item && isHovered && (
         <div className="absolute top-20 left-1/2 transform -translate-x-1/2 bg-gray-800 text-white p-4 rounded-lg shadow-lg w-72 z-50 text-center">
           <img
-            src={`rise-online-calculator/assets/${props.item.name.toLowerCase()}.png`}
+            src={image}
             alt={props.item.name}
             className="h-12 w-12 object-cover rounded-md mx-auto"
           />
